8b4d8f90eb128a8e5bd6b2a04d412c31161c81e1
Georgios Fotopoulos <geo.fot@gmail.com>
2023-07-08 11:46:45 +0300

    Add files via upload

---
+++ 1. Data processing/csv_split.py
@@ -0,0 +1,10 @@
+# import libraries
+import pandas as pd
+
+# split dataset into batches
+chunk_size = 500000
+batch_no = 1
+
+for chunk in pd.read_csv('/Users/GeoFot/data/yelp_review.csv', chunksize=chunk_size, delimiter=','):
+    chunk.to_csv('yelp_review' + str(batch_no) + '.csv', index=False)
+    batch_no +=1

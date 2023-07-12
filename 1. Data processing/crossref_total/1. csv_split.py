8a54dd6d23d4532ecbdf14999340d3a5ed5931db
Paris Koloveas <pkoloveas@uop.gr>
2023-07-13 01:47:57 +0200

    Update 1. csv_split.py

--- 1. Data processing/crossref_total/1. csv_split.py
+++ 1. Data processing/crossref_total/1. csv_split.py
@@ -5,6 +5,6 @@ import pandas as pd
 chunk_size = 500000
 batch_no = 1
 
-for chunk in pd.read_csv('/Users/GeoFot/data/crossref_total/crossref_total.csv', chunksize=chunk_size, delimiter=',', encoding = "ISO-8859-1"):
+for chunk in pd.read_csv('../../data/crossref/crossref_total.csv', chunksize=chunk_size, delimiter=',', encoding = "ISO-8859-1"):
     chunk.to_csv('crossref_total' + str(batch_no) + '.csv', index=False)
     batch_no +=1
